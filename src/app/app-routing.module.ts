0764545ba0a35231d749d3c36c9969fd5662a280
Esvin Ambrocio <esvinambrocioc@gmail.com>
2022-02-21 18:42:38 -0600

    Modificar el app-routing.module.ts para tener la ruta predeterminada apuntando a “home” y las respectivas rutas para “home” y “perfil” de tal forma que podamos acceder mediante las ruta /home y /perfil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { PerfilComponent } from "./perfil/perfil.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'perfil',
+    component: PerfilComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
